de18095cbf185c9fc18ce49c7e40f70d20e44ca0
Sinan Kaya <skaya.sinan@outlook.com>
2025-08-14 15:18:45 +0300

    _added component

---
+++ Portfolio.Web/ViewComponents/AdminLayout/_AdminLayoutBreadCrumbComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutBreadCrumbComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
