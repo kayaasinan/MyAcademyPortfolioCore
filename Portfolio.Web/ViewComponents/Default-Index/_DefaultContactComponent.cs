440d416ed11fed9ada4b6dd131c1a25b85c6407a
Sinan Kaya <skaya.sinan@outlook.com>
2025-08-21 22:16:19 +0300

    _added new entities

---
+++ Portfolio.Web/ViewComponents/Default-Index/_DefaultContactComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Web.Context;
+
+namespace Portfolio.Web.ViewComponents.Default_Index
+{
+    public class _DefaultContactComponent(PortfolioContext context) : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var contact=context.ContactInfos.ToList();
+            return View(contact);
+        }
+    }
+}
