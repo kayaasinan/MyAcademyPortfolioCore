2c33b32ea5fae36d2ce24de8b1e4bd31324291bd
Sinan Kaya <skaya.sinan@outlook.com>
2025-08-28 15:40:36 +0300

    _

---
+++ Portfolio.Web/ViewComponents/Default-Index/_DefaultTestimonialsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.ViewComponents.Default_Index
+{
+    public class _DefaultTestimonialsComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
